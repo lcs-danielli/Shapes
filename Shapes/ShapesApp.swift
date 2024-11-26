b86473de1b69a040f7884a896490290decdc1b77
Daniel Li <danli@lcs.on.ca>
2024-11-26 14:20:33 -0500

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by 李泽宇 on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
